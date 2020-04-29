4c58f6d52593d5cb072f702427be0c80ad393ec0
Mike <lanselot777hailander@gmail.com>
2020-04-28 22:47:10 -0400

    Adding home page

---
+++ componentes/lib/src/pages/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Components'),
+      ),
+      body: _lista(),
+    );
+  }
+
+  Widget _lista() {
+
+    return ListView(
+      children: _listaItems(),
+    );
+
+  }
+
+  List<Widget> _listaItems() {
+    return [
+      ListTile(title: Text('Hola Mundo'),),
+      Divider(),
+      ListTile(title: Text('Hola Mundo'),),
+      Divider(),
+      ListTile(title: Text('Hola Mundo'),)
+    ];
+  }
+
+}
--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget{
 
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,21 +11,49 @@ class HomePageTemp extends StatelessWidget{
         title: Text('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('List Tile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('List Tile Title'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('List Tile Title'),
-          ),
-        ],
+        //children: _crearItems()
+        children: _crearItemsCorta()
       ),
     );
+  }
+
+  List<Widget> _crearItems() {
+
+    List<Widget> lista = new List<Widget>();
+
+    for (var opt in opciones) {
+      
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+
+      lista..add(tempWidget)
+           ..add(Divider());      
+
+    }
+
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta(){
+
+    return opciones.map((item){
+
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){},
+          ),
+          Divider()
+        ],
+      );
+
+    }).toList();    
+
   }
 
 }
