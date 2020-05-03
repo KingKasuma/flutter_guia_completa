1d0cc8769a07f38467f8d5f4fda195f6a83fa118
Mike <lanselot777hailander@gmail.com>
2020-05-03 18:10:45 -0400

    Input Text page created

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/card_page.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
@@ -13,7 +14,8 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'alert'   : ( BuildContext context ) => AlertPage(),
     'avatar'  : ( BuildContext context ) => AvatarPage(),
     'card'    : ( BuildContext context ) => CardPage(),
-    'animatedContainer'    : ( BuildContext context ) => AnimatedContainerPage()
+    'animatedContainer'    : ( BuildContext context ) => AnimatedContainerPage(),
+    'inputs'    : ( BuildContext context ) => InputPage(),
   };
 
 }
