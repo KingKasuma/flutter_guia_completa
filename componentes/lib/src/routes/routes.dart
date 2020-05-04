e7c61f01be99949c3f9496b201c648706f15f0b1
Mike <lanselot777hailander@gmail.com>
2020-05-03 20:51:42 -0400

    List View Builder created

--- componentes/lib/src/routes/routes.dart
+++ componentes/lib/src/routes/routes.dart
@@ -1,6 +1,7 @@
 import 'package:componentes/src/pages/animated_container.dart';
 import 'package:componentes/src/pages/card_page.dart';
 import 'package:componentes/src/pages/input_page.dart';
+import 'package:componentes/src/pages/listview_page.dart';
 import 'package:componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 
@@ -18,6 +19,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     'animatedContainer'    : ( BuildContext context ) => AnimatedContainerPage(),
     'inputs'    : ( BuildContext context ) => InputPage(),
     'slider'    : ( BuildContext context ) => SliderPage(),
+    'list'    : ( BuildContext context ) => ListaPage(),
   };
 
 }
