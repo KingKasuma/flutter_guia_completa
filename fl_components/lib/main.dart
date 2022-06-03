7475e8adfce7833c32a20766ac978193132d37df
Mike <lanselot777hailander@gmail.com>
2022-06-03 15:28:28 -0400

    Fixes

--- fl_components/lib/main.dart
+++ fl_components/lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:fl_components/router/app_routes.dart';
 import 'package:flutter/material.dart';
-import 'package:fl_components/screens/screens.dart';
 
 void main() => runApp(const MyApp());
 
@@ -9,23 +9,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      //home: const CardScreen(),
-      initialRoute: 'home',
-      routes: {
-        'home': (BuildContext context) => const HomeScreen(),
-        'listview1': (BuildContext context) => const Listview1Screen(),
-        'listview2': (BuildContext context) => const Listview2Screen(),
-        'alert': (BuildContext context) => const AlertScreen(),
-        'card': (BuildContext context) => const CardScreen(),
-      },
-      onGenerateRoute: (settings) {
-        print(settings);
-        return MaterialPageRoute(
-          builder: (context) => const AlertScreen(),
-        );
-      },
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: AppRoutes.initialRoute,
+        routes: AppRoutes.getAppRoutes(),
+        onGenerateRoute: (settings) => AppRoutes.onGenerateRoute(settings));
   }
 }
--- fl_components/lib/screens/home_screen.dart
+++ fl_components/lib/screens/home_screen.dart
@@ -1,11 +1,13 @@
-import 'package:fl_components/screens/screens.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_components/router/app_routes.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final menuOptions = AppRoutes.menuOptions;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Componentes en Flutter'),
@@ -13,18 +15,21 @@ class HomeScreen extends StatelessWidget {
       ),
       body: ListView.separated(
           itemBuilder: (context, index) => ListTile(
-                leading: const Icon(Icons.access_time_outlined),
-                title: const Text('Nombre de rute'),
+                leading: Icon(
+                  menuOptions[index].icon,
+                  color: Colors.indigo,
+                ),
+                title: Text(menuOptions[index].name),
                 onTap: () {
                   // final route = MaterialPageRoute(
                   //     builder: (context) => const Listview1Screen()
                   // );
                   // Navigator.push(context, route);
-                  Navigator.pushNamed(context, 'alert');
+                  Navigator.pushNamed(context, menuOptions[index].route);
                 },
               ),
           separatorBuilder: (_, __) => const Divider(),
-          itemCount: 100),
+          itemCount: menuOptions.length),
     );
   }
 }
