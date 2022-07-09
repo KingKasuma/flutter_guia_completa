a7e74365f317a281e6b54f046a727e6c234476c5
Mike <lanselot777hailander@gmail.com>
2022-07-09 10:07:19 -0400

    Fixin input texts

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class InputsScreen extends StatelessWidget {
   const InputsScreen({Key? key}) : super(key: key);
 
@@ -13,32 +15,10 @@ class InputsScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              TextFormField(
-                autofocus: false,
-                initialValue: '',
-                textCapitalization: TextCapitalization.words,
-                onChanged: (value) {
-                  print('value: $value');
-                },
-                validator: (value) {
-                  if (value == null) return 'Este campo es requerido';
-                  return value.length < 3 ? 'Minimo de 3 letras' : null;
-                },
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: InputDecoration(
-                    hintText: 'Nombre del usuario',
-                    labelText: 'Nombre',
-                    helperText: 'Solo letras',
-                    // prefix: Icon(Icons.verified_user_outlined),
-                    suffixIcon: Icon(Icons.group_outlined),
-                    icon: Icon(Icons.assignment_ind_outlined),
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.green)),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(10),
-                            topRight: Radius.circular(10)))),
+            children: const [
+              CustomInputField(
+                labelText: 'Nombre',
+                hintText: 'Nombre del usuario',
               )
             ],
           ),
