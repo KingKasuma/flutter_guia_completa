5d51a88d767629cc4d3857f31c9b331880e6880c
Mike <lanselot777hailander@gmail.com>
2022-07-14 16:31:12 -0400

    email and password fields

--- fl_components/lib/screens/inputs_screen.dart
+++ fl_components/lib/screens/inputs_screen.dart
@@ -19,7 +19,25 @@ class InputsScreen extends StatelessWidget {
               CustomInputField(
                 labelText: 'Nombre',
                 hintText: 'Nombre del usuario',
-              )
+              ),
+              SizedBox(height: 30),
+              CustomInputField(
+                labelText: 'Apellido',
+                hintText: 'Apellido del usuario',
+              ),
+              SizedBox(height: 30),
+              CustomInputField(
+                labelText: 'Correo',
+                hintText: 'Correo del usuario',
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 30),
+              CustomInputField(
+                labelText: 'Contraseña',
+                hintText: 'Contraseña del usuario',
+                obscureText: true,
+              ),
+              SizedBox(height: 30),
             ],
           ),
         ),
